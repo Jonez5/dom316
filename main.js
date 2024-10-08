1ae2bec82822780eaade6ee9797c76c5784306b8
Jonez5 <elijahadell8@gmail.com>
2024-10-08 15:39:32 -0400

    part

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ mainEl.style.backgroundColor =  "#4a4e4d";
  header.textContent = 'DOM Manipiulation';
  mainEl.append(header)
  mainEl.classList.add('flex-ctr')
+
+
+ let topMenuEl = document.getElementById('top-menu')
+ topMenuEl.style.height = '100%';
+ topMenuEl.style.backgroundColor = "#0e9aa7";
+ topMenuEl.classList.add('flex-around')
+
+
+ 
