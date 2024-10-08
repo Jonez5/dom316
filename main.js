f0e3371a974b93c6615ad0c67cebdff2c0f82b9b
Jonez5 <elijahadell8@gmail.com>
2024-10-08 14:59:08 -0400

    part1

---
+++ main.js
@@ -0,0 +1,7 @@
+
+let mainEl = document.querySelector("main")
+mainEl.style.backgroundColor =  "#4a4e4d";
+ let header = document.createElement('h1')
+ header.textContent = 'DOM Manipiulation';
+ mainEl.append(header)
+ mainEl.classList.add('flex-ctr')
